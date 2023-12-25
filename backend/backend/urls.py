32013a56a94fecbfd8757d9b2ff6e6ac3f988269
Hassan Shahzad Aheer <hassanshahzadaheer@gmail.com>
2023-12-25 18:59:54 +0500

    Update urls.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from base import urls as base_urls
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('base.urls')),
+    path('', include(base_urls)),
 ]
